48fa2df3ef6c7a1a62776dd823ec5b36b0aaf9c9
盛俊鹏 <shengjunpeng@yiwise.com>
2023-02-17 15:26:45 +0800

    1

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:CFsjp/CFsjp.github.io.git master:master
+
+cd -

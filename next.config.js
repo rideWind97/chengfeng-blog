9ebf3a87d7e87678a09cf25c654d5abdd2bc149b
盛俊鹏 <shengjunpeng@yiwise.com>
2023-02-17 15:23:50 +0800

    feat:博客更新

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

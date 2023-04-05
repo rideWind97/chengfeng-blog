1f88418fffa491dbad17d052683d749f8c6ebc98
盛俊鹏 <shengjunpeng@yiwise.com>
2023-04-05 16:52:44 +0800

    fix: 2

--- 1.js
+++ 1.js
@@ -1,16 +1,204 @@
-function getBeijingtime() {
-  //获得当前运行环境时间
-  let d = new Date();
-  let currentDate = new Date();
-  let tmpHours = currentDate.getHours();
-  //算得时区
-  let time_zone = -d.getTimezoneOffset() / 60;
-  if (time_zone < 0) {
-      time_zone = Math.abs(time_zone) + 8; currentDate.setHours(tmpHours + time_zone);
-  } else {
-      time_zone -= 8; currentDate.setHours(tmpHours - time_zone);
-  }
-  return currentDate;
-}
-
-console.log(getBeijingtime().getMinutes())
+const a = {
+  ref: "refs/heads/main",
+  before: "5636a1e1ebd55755a35f18fb41beb52a0915c878",
+  after: "c15de85737b680205372da42ebfaec058cd9f031",
+  repository: {
+    id: 485226772,
+    node_id: "R_kgDOHOv5FA",
+    name: "chengfeng-blog",
+    full_name: "rideWind97/chengfeng-blog",
+    private: false,
+    owner: {
+      name: "rideWind97",
+      email: "47934820+rideWind97@users.noreply.github.com",
+      login: "rideWind97",
+      id: 47934820,
+      node_id: "MDQ6VXNlcjQ3OTM0ODIw",
+      avatar_url: "https://avatars.githubusercontent.com/u/47934820?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/rideWind97",
+      html_url: "https://github.com/rideWind97",
+      followers_url: "https://api.github.com/users/rideWind97/followers",
+      following_url:
+        "https://api.github.com/users/rideWind97/following{/other_user}",
+      gists_url: "https://api.github.com/users/rideWind97/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/rideWind97/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/rideWind97/subscriptions",
+      organizations_url: "https://api.github.com/users/rideWind97/orgs",
+      repos_url: "https://api.github.com/users/rideWind97/repos",
+      events_url: "https://api.github.com/users/rideWind97/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/rideWind97/received_events",
+      type: "User",
+      site_admin: false,
+    },
+    html_url: "https://github.com/rideWind97/chengfeng-blog",
+    description: null,
+    fork: false,
+    url: "https://github.com/rideWind97/chengfeng-blog",
+    forks_url: "https://api.github.com/repos/rideWind97/chengfeng-blog/forks",
+    keys_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/keys{/key_id}",
+    collaborators_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/collaborators{/collaborator}",
+    teams_url: "https://api.github.com/repos/rideWind97/chengfeng-blog/teams",
+    hooks_url: "https://api.github.com/repos/rideWind97/chengfeng-blog/hooks",
+    issue_events_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/issues/events{/number}",
+    events_url: "https://api.github.com/repos/rideWind97/chengfeng-blog/events",
+    assignees_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/assignees{/user}",
+    branches_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/branches{/branch}",
+    tags_url: "https://api.github.com/repos/rideWind97/chengfeng-blog/tags",
+    blobs_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/git/blobs{/sha}",
+    git_tags_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/git/tags{/sha}",
+    git_refs_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/git/refs{/sha}",
+    trees_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/git/trees{/sha}",
+    statuses_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/statuses/{sha}",
+    languages_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/languages",
+    stargazers_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/stargazers",
+    contributors_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/contributors",
+    subscribers_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/subscribers",
+    subscription_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/subscription",
+    commits_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/commits{/sha}",
+    git_commits_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/git/commits{/sha}",
+    comments_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/comments{/number}",
+    issue_comment_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/issues/comments{/number}",
+    contents_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/contents/{+path}",
+    compare_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/compare/{base}...{head}",
+    merges_url: "https://api.github.com/repos/rideWind97/chengfeng-blog/merges",
+    archive_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/{archive_format}{/ref}",
+    downloads_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/downloads",
+    issues_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/issues{/number}",
+    pulls_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/pulls{/number}",
+    milestones_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/milestones{/number}",
+    notifications_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/notifications{?since,all,participating}",
+    labels_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/labels{/name}",
+    releases_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/releases{/id}",
+    deployments_url:
+      "https://api.github.com/repos/rideWind97/chengfeng-blog/deployments",
+    created_at: 1650862234,
+    updated_at: "2023-02-24T00:57:30Z",
+    pushed_at: 1680684575,
+    git_url: "git://github.com/rideWind97/chengfeng-blog.git",
+    ssh_url: "git@github.com:rideWind97/chengfeng-blog.git",
+    clone_url: "https://github.com/rideWind97/chengfeng-blog.git",
+    svn_url: "https://github.com/rideWind97/chengfeng-blog",
+    homepage: null,
+    size: 4490,
+    stargazers_count: 2,
+    watchers_count: 2,
+    language: null,
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    has_discussions: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 2,
+    license: null,
+    allow_forking: true,
+    is_template: false,
+    web_commit_signoff_required: false,
+    topics: [],
+    visibility: "public",
+    forks: 0,
+    open_issues: 2,
+    watchers: 2,
+    default_branch: "main",
+    stargazers: 2,
+    master_branch: "main",
+  },
+  pusher: {
+    name: "rideWind97",
+    email: "47934820+rideWind97@users.noreply.github.com",
+  },
+  sender: {
+    login: "rideWind97",
+    id: 47934820,
+    node_id: "MDQ6VXNlcjQ3OTM0ODIw",
+    avatar_url: "https://avatars.githubusercontent.com/u/47934820?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/rideWind97",
+    html_url: "https://github.com/rideWind97",
+    followers_url: "https://api.github.com/users/rideWind97/followers",
+    following_url:
+      "https://api.github.com/users/rideWind97/following{/other_user}",
+    gists_url: "https://api.github.com/users/rideWind97/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/rideWind97/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/rideWind97/subscriptions",
+    organizations_url: "https://api.github.com/users/rideWind97/orgs",
+    repos_url: "https://api.github.com/users/rideWind97/repos",
+    events_url: "https://api.github.com/users/rideWind97/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/rideWind97/received_events",
+    type: "User",
+    site_admin: false,
+  },
+  created: false,
+  deleted: false,
+  forced: false,
+  base_ref: null,
+  compare:
+    "https://github.com/rideWind97/chengfeng-blog/compare/5636a1e1ebd5...c15de85737b6",
+  commits: [
+    {
+      id: "c15de85737b680205372da42ebfaec058cd9f031",
+      tree_id: "36a9c13e51e6cdfa23b891fcad348297090c864e",
+      distinct: true,
+      message: "fix: 1",
+      timestamp: "2023-04-05T16:49:24+08:00",
+      url: "https://github.com/rideWind97/chengfeng-blog/commit/c15de85737b680205372da42ebfaec058cd9f031",
+      author: [Object],
+      committer: [Object],
+      added: [Array],
+      removed: [],
+      modified: [Array],
+    },
+  ],
+  head_commit: {
+    id: "c15de85737b680205372da42ebfaec058cd9f031",
+    tree_id: "36a9c13e51e6cdfa23b891fcad348297090c864e",
+    distinct: true,
+    message: "fix: 1",
+    timestamp: "2023-04-05T16:49:24+08:00",
+    url: "https://github.com/rideWind97/chengfeng-blog/commit/c15de85737b680205372da42ebfaec058cd9f031",
+    author: { name: "盛俊鹏", email: "shengjunpeng@yiwise.com" },
+    committer: { name: "盛俊鹏", email: "shengjunpeng@yiwise.com" },
+    added: ["1.js"],
+    removed: [],
+    modified: [".github/workflows/develop.yml"],
+  },
+};
